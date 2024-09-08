c9b62496bca966a8264e07111e5756f72cef8450
Omotola Odusanya <odusanyaomotola64@gmail.com>
2024-09-08 07:32:55 +0100

    description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "FF-RafleDraw",
-  description: "Flying Fish Rafle Draw",
+  title: "TolaRafleDraw",
+  description: "Tola Rafle Draw",
 };
 
 export default function RootLayout({
